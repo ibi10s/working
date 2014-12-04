2037943256e406856fcbf445c413667a35a547b0
vagrant <vagrant@chef-workstation2.(none)>
2014-12-04 12:45:34 +0000

    first commit

---
+++ cookbooks/navrabbit/recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: navrabbit
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+include_recipe 'rabbitmq'
+include_recipe 'rabbitmq::mgmt_console'
